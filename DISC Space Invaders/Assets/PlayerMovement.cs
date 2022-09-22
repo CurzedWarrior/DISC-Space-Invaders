82177dc9a8cd9f2e86a1c2b98255363c2cd2fada
CursedWarrior <86751297+CurzedWarrior@users.noreply.github.com>
2022-09-22 18:55:41 +1200

    Movement
    Simple left and right movement

---
+++ DISC Space Invaders/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = 5f;
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+    }
+}
